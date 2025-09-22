b598b6d2d94e0b09d021b30820fb33ef3495b5be
George Kreye <102697450+GeorgeKreye@users.noreply.github.com>
2025-09-22 12:40:39 -0600

    Bridge setup commands

--- lab1/part1/do-lab.sh
+++ lab1/part1/do-lab.sh
@@ -3,3 +3,12 @@
 # INCLUDE ALL COMMANDS NEEDED TO PERFORM THE LAB
 # This file will get called from capture_submission.sh
 
+# create bridge
+sw ip link add name lab-bridge type bridge
+sw ip link set lab-bridge up
+
+# slave hosts to bridge
+sw ip link set eth1 master lab-bridge
+sw ip link set eth2 master lab-bridge
+sw ip link set eth3 master lab-bridge
+sw ip link set eth4 master lab-bridge
