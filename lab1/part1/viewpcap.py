7f84d8384a69267fdd8ad9f41f8c611e72edba39
George Kreye <102697450+GeorgeKreye@users.noreply.github.com>
2025-09-25 17:29:43 -0600

    Update viewpcap.py

--- lab1/part1/viewpcap.py
+++ lab1/part1/viewpcap.py
@@ -1,21 +1,50 @@
 #!/usr/bin/env python3
 
-from scapy.all import *
+import argparse
+import scapy.all as scapy
 
 
 def print_pkts(pkts):
-    counter = 0
+    """
+    Iterates through packets in a read-in pcap file, printing them to console.
+    """
+    # Counter for display of packet order
+    counter = 1
+
+    # Iterate through packets
     for pkt in pkts:
-        print(f"=PACKET {counter}").center(10,'=')
+        # Print packet
+        print(f"PACKET {counter}".center(20,'='))
+        pkt.show()
+
+        # Iterate packet order counter
         counter += 1
 
-def main():
-    path = input("Enter pcap filepath (either relative from PWD or absolute):\n")
-    pkts = rdpcap(path)
+def main(path):
+    """
+    Main function. Reads in pcap file, and runs function to print packets
+    if non-empty.
+    """
+    # Read in pcap file from path
+    pkts = scapy.rdpcap(path)
+
+    # Make sure pcap file isn't empty
     if len(pkts) > 0:
+        # Print packets
         print_pkts(pkts)
     else:
         print("Pcap file is empty!")
 
+# Script execution
 if __name__ == "__main__":
-    main()
+    # Handle command-line arguments
+    parser = argparse.ArgumentParser(description="Reads out a pcap file to console.")
+    parser.add_argument(
+        "path",
+        type=str,
+        help="Path to pcap file to read (either relative to PWD or absolute)"
+    )
+    args = parser.parse_args()
+
+    # Run script
+    main(args.path)
