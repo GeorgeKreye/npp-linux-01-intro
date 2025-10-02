41993d80eb36a5ca01da199d2de900f67711852e
George Kreye <102697450+GeorgeKreye@users.noreply.github.com>
2025-10-02 09:21:31 -0600

    Update run.sh
    Don't want to bloat bashrc per run + clab folder cleanup

--- lab1/part1/run.sh
+++ lab1/part1/run.sh
@@ -4,9 +4,8 @@
 echo "Creating needed directories for lab"
 ./provided/make_dirs.sh
 
-# Ensure creation of aliases
-echo "Creating aliases"
-./provided/make_aliases.sh
+# Warning about aliases
+echo "WARNING: Script will not function properly if aliases are not created. If unindended behavior occurs, run ./provided/make_aliases.sh to add needed aliases to bashrc and try again."
 
 # Run lab
 ./provided/capture_submission.sh
@@ -25,3 +24,4 @@ echo "Moved captures to directory $sub"
 # cleanup
 echo "Cleaning up"
 ./rm_dirs.sh
+sudo rm -r clab-lab1-part1
